8c20ad2cbaaecce6021bdc1c44125617dc0ef747
Ntpsrm <ntpsrm.md@gmail.com>
2024-11-08 11:25:56 +0700

    test path

--- js/callFecth.js
+++ js/callFecth.js
@@ -1,24 +1,23 @@
+
 $(document).ready(async function () {
     $('html *').each(async function () {
-        const element = $(this);
-        if (!element.attr('id')) return;
-        const paths = [
-            `/pages/${element.attr('id')}.html`,
-            `/component/${element.attr('id')}.html`,
-            `https://ntp-md.github.io/mod-tools/component/${element.attr('id')}.html`
-        ];
+            const element = $(this);
+            if (!element.attr('id')) return;
+            const paths = [
+                    `/pages/${element.attr('id')}.html`,
+                    `/component/${element.attr('id')}.html`,
+                    `https://ntp-md.github.io/mod-tools/component/${element.attr('id')}.html`
+            ];
 
-        for (const filePath of paths) {
-            try {
-                const response = await $.get(filePath);
-                element.html(response);
-                break; // Stop trying other paths
-            } catch (error) {
-                console.error(`Failed to load from path: ${filePath}`, error);
+            for (const filePath of paths) {
+                    try {
+                            const response = await $.get(filePath);
+                            element.html(response);
+                            break; // Stop trying other paths
+                    } catch (error) {
+                            console.error(`Failed to load from path: ${filePath}`, error);
+                    }
             }
-        }
     });
 });
-
-
 
