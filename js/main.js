25ce01001be63ac82ce4a462efd0c6c8d80569e8
Ntpsrm <ntpsrm.md@gmail.com>
2024-11-08 11:28:38 +0700

    test path

--- js/main.js
+++ js/main.js
@@ -10,26 +10,6 @@ $(document).on('click', function (e) {
 });
 
 
-$(document).ready(function () {
-    if (localStorage.getItem('darkMode') === 'enabled') {
-        $('body').addClass('dark-mode');
-    }
-});
-
-
-$(document).on('click', function (e) {
-    if ($(e.target).closest('.toggle-mode').length) {
-        $('body').toggleClass('dark-mode');
-
-        // Save the mode in local storage
-        if ($('body').hasClass('dark-mode')) {
-            localStorage.setItem('darkMode', 'enabled');
-        } else {
-            localStorage.setItem('darkMode', 'disabled');
-        }
-    }
-});
-
 
 
 
